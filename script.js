40175f0f4aff2b7a422c6388fc84b6439ed9208e
Matt5344 <rasclematteoga@gmail.com>
2024-08-26 11:06:41 +0200

    mise à jour du site

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  
+//POUR LA SECTION DES DIFFERENTS PROJETS
+  let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
